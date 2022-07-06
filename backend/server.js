47f475f29de6c1b014da0b6fe169d3e0ef63db5b
Hieu_히에우 <hieu28996@gmail.com>
2022-07-06 15:54:07 +0700

    authentication

--- backend/server.js
+++ backend/server.js
@@ -3,31 +3,89 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
-const properties = require("./config/properties");
-const apiRoutes = require("./routes/routes");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
 const bodyParserJSON = bodyParser.json();
 const bodyParserURLEncoded = bodyParser.urlencoded({ extended: true });
 const router = express.Router();
-const whitelist = properties.CORS;
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+// const whitelist = properties.CORS;
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParserJSON);
 app.use(morgan("common"));
 app.use(bodyParserURLEncoded);
 app.use("/api", router);
-apiRoutes(router);
 
-app.listen(properties.PORT, (req, res) => {
-  console.log(`Server is running on ${properties.PORT} port.`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT} port.`);
 });
+
+// connect database
+const db = require("./models");
+const Role = db.role;
+
+mongoose
+  .connect((process.env.MONGODB_URL), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+  function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+      if (!err && count === 0) {
+        new Role({
+          name: "user",
+        }).save((err) => {
+          if (err) {
+            console.log("error", err);
+          }
+
+          console.log("added 'user' to roles collection");
+        });
+
+        new Role({
+          name: "moderator",
+        }).save((err) => {
+          if (err) {
+            console.log("error", err);
+          }
+
+          console.log("added 'moderator' to roles collection");
+        });
+
+        new Role({
+          name: "admin",
+        }).save((err) => {
+          if (err) {
+            console.log("error", err);
+          }
+
+          console.log("added 'admin' to roles collection");
+        });
+      }
+    });
+  }
+  initial;
+
+  // require("./routes/auth.routes")(app);
+  // require("./routes/user.routes")(app);
