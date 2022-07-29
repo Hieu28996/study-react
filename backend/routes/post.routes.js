18be4404bb64354a6c111debc6570746fd988f45
Hieu_히에우 <hieu28996@gmail.com>
2022-07-29 17:01:00 +0700

    update post api

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,11 +1,10 @@
 const router = require("express").Router();
-const { authJwt, verifyEditUser } = require("../middlewares");
+const { authJwt } = require("../middlewares");
 const controller = require("../controllers/post.controller");
 
-router.post("/create",
-  authJwt.verifyToken,
-  controller.createPost);
-
 router.get("/all", controller.allPost);
+router.get("/post", authJwt.verifyToken, controller.getPost);
+router.post("/create", authJwt.verifyToken, controller.createPost);
+router.delete("/delete", authJwt.verifyToken, controller.deletePost);
 
 module.exports = router;
